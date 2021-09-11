1a07e6f7482d6e390513cfb5383270f7b877de41
Kristel Juen <kristeljuen@gmail.com>
2021-09-11 10:31:18 +0800

    updating local dev

--- config.php
+++ config.php
@@ -12,11 +12,6 @@ $db_name = "useraccounts";
 //$charset = 'utf8';
 
 //$dsn = "mysql:host=$host;dbname=$db_name;charset=$charset";
-
 $db = new PDO('mysql:host=localhost;dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass);
-
 //$db = new PDO($dsn, $db_user, $db_pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-//NOTE NA UPLOAD NA NI SA HEROKU
