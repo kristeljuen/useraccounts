bfa4608c7cb8c9fd0edb8de4945f3261bf3027c6
Kristel Juen <kristeljuen@gmail.com>
2021-09-11 10:48:14 +0800

    local update
    cant login in mysql.com

--- config.php
+++ config.php
@@ -1,17 +1,17 @@
 <?php
 // Development Connection
-//$db_user = "root";
-//$db_pass = "";
-//$db_name = "useraccounts";
+$db_user = "root";
+$db_pass = "";
+$db_name = "useraccounts";
 
 //Remote Database Connection
-$host = 'remotemysql.com';
-$db_name = 'dgPXnrqmh2';
-$db_user = 'dgPXnrqmh2';
-$db_pass = 'zRaIOnGspo';
-$charset = 'utf8';
+//$host = 'remotemysql.com';
+//$db_name = 'dgPXnrqmh2';
+//$db_user = 'dgPXnrqmh2';
+//$db_pass = 'zRaIOnGspo';
+//$charset = 'utf8';
 
-$dsn = "mysql:host=$host;dbname=$db_name;charset=$charset";
-//$db = new PDO('mysql:host=localhost;dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass);
-$db = new PDO($dsn, $db_user, $db_pass);
+//$dsn = "mysql:host=$host;dbname=$db_name;charset=$charset";
+$db = new PDO('mysql:host=localhost;dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass);
+//$db = new PDO($dsn, $db_user, $db_pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
